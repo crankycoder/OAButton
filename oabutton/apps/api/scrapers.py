d26a9bd4d2aa16e76bc41cb6917fc5f705021cd5
Victor Ng <victor@crankycoder.com>
2013-12-07 22:24:53 -0500

    added a bunch of email processing code for grabbing email from imap

---
+++ oabutton/apps/api/scrapers.py
@@ -0,0 +1,59 @@
+import BeautifulSoup
+
+import imaplib
+import email
+
+def check_inbox():
+    """
+    Check inbound mail
+    """
+    mail = imaplib.IMAP4_SSL('imap.gmail.com')
+    mail.login('oabutton@crankycoder.com', 'hackoabutton')
+    mail.select("inbox") # connect to inbox.
+
+    result, data = mail.uid('search', None, "ALL") # search and return uids instead
+    # the result should always come back with 'OK'
+    uids = data[0].split()
+    for uid in uids:
+        raw_email = load_mail_by_uid(mail, uid)
+        mail_dict = process_raw_email(raw_email)
+        print "--------"
+        print "From: %s <%s>" % mail_dict['from']
+        print "To: %s <%s>" % mail_dict['to']
+        print "Subject: %s" % mail_dict['subject']
+        print mail_dict['text']
+
+
+def process_raw_email(raw_email):
+    """
+    Return a dictionary with the following keys:
+    """
+    email_message = email.message_from_string(raw_email)
+    result = {}
+    result['to'] = email.utils.parseaddr(email_message['To'])
+    result['from'] = email.utils.parseaddr(email_message['From'])
+    result['subject'] = email_message['Subject']
+    result['headers'] = email_message.items() # print all headers
+    result['text'] = extract_text(email_message)
+    return result
+
+def extract_text(email_message_instance):
+    maintype = email_message_instance.get_content_maintype()
+    if maintype == 'multipart':
+        for part in email_message_instance.get_payload():
+            if part.get_content_maintype() == 'text':
+                return part.get_payload()
+    elif maintype == 'text':
+        return email_message_instance.get_payload()
+
+
+def load_mail_by_uid(mail, uid):
+    """return the raw email"""
+    result, data = mail.uid('fetch', uid, '(RFC822)')
+    return data[0][1]
+
+def scrape_email(html_text):
+    pass
+
+check_inbox()
+
