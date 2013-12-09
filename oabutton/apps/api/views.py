df64a8a74f0371b3e72a3be814d180d2e35592b1
Victor Ng <victor@crankycoder.com>
2013-12-09 06:59:27 -0500

    added template loading for addition of new open access document for success and failure modes

--- oabutton/apps/api/views.py
+++ oabutton/apps/api/views.py
@@ -1,21 +1,31 @@
 # Create your views here.
 from models import PendingOpen
 from datetime import datetime
+from django.shortcuts import render_to_response
 
 def add_doc(req, secret):
-    results = PendingOpen.objects.filter(verfication_secret = secret)
-    if len(results) > 0:
-        for evt in results:
-            for l in evt.email_text.split('\n'):
-                if l.strip().startswith("http"):
-                    url = l.strip()
-                    doi = ''.join(evt.subject.split()[2:])
 
-                    # TODO: verify that the URL we're adding is
-                    # actually live
+    for pending in PendingOpen.objects.filter(verfication_secret = secret):
+        for l in pending.email_text.split('\n'):
+            if l.strip().startswith("http"):
+                url = l.strip()
 
-                    OpenDocument.objects.create(doi=doi, oa_url=url,
-                            release_email = evt.sender_email,
-                            date_opened=datetime.now())
+                # Extract the DOI, this should've been done before
+                # creating the PendingOpen object
+                doi = ''.join(evt.subject.split()[2:])
 
-                    evt.delete()
+                # TODO: verify that the URL we're adding is
+                # actually live
+
+                OpenDocument.objects.create(doi=doi, oa_url=url,
+                        release_email = evt.sender_email,
+                        date_opened=datetime.now())
+
+                evt.delete()
+
+                # TODO: add document open success
+                return render_to_response('api/document_open_success.html')
+
+    # TODO: add document open failure template
+    return render_to_response('api/document_open_fail.html')
+
